4ebf54af0a3e99770c31175af4b0a6dc9b3c7a25
ProgrammerAtik <atik.khan3477@gmail.com>
2026-02-08 13:05:57 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JAVA_FX
+Equation Plotter App
---
+++ src/main/java/org/example/MainApp.java
@@ -0,0 +1,34 @@
+package com.equationplotter;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        BorderPane root = new BorderPane();
+
+        Label title = new Label("Equation Plotter (JavaFX) - Window Ready");
+        title.setStyle("-fx-font-size: 20px; -fx-font-weight: bold;");
+
+        root.setTop(title);
+        BorderPane.setMargin(title, new Insets(20));
+
+        Scene scene = new Scene(root, 1000, 650);
+
+        stage.setTitle("Equation Plotter");
+        stage.setScene(scene);
+        stage.setMinWidth(800);
+        stage.setMinHeight(500);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
